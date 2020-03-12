27e3d7f1d06fcfdc57ee7b2df52379ca12db7f3c
Nicolas Nunez <nicolasnunezromay@gmail.com>
2020-03-12 16:42:24 -0500

    Moved resources files to another direcotory and updated RESOURCES variables to read from there

--- framework/properties.py
+++ framework/properties.py
@@ -1,7 +1,7 @@
 import glob
 import os
 ROOT = os.path.abspath(os.path.dirname(__file__))
-RESOURCES = os.path.join(ROOT, 'conf')
+RESOURCES = os.path.join(ROOT, 'resources')
 _files = glob.glob(f'{RESOURCES}/*.ini')
 _reports = os.path.join(ROOT, 'data/reports')
 csv_dir = os.path.join(_reports, 'csv')
